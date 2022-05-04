0bb870f11ea39bc3a2c2f86cefa6e80031c27f3c
Gabriel Gregorio <76271518+Gregab1@users.noreply.github.com>
2022-05-04 13:47:57 -0300

    Add files via upload

---
+++ semana3/Controlador.java
@@ -0,0 +1,8 @@
+package semana3;
+
+public class Controlador {
+    public static void main(String[] args) {
+        Persona Persona1=Vista.EntregarPersona();
+        System.out.println(Vista.MostarPersona(Persona1));
+    }
+}
---
+++ semana3/Vista.java
@@ -0,0 +1,31 @@
+package semana3;
+
+import java.util.Scanner;
+
+public class Vista {
+    public static  Persona EntregarPersona(){
+        Scanner Leer=new Scanner(System.in);
+        String nom,apellido;
+
+        System.out.println("ingrese su nombre");
+        nom=Leer.nextLine();
+        Persona persona =new Persona ();
+        persona.setNombre(nom);
+        System.out.println("ingrese su apellido");
+        apellido=Leer.nextLine();
+        persona.setApellido(apellido);
+        return persona;
+
+    }
+
+    public static String MostarPersona(Persona per1){
+
+
+
+        return "los dato ingresados son;"+
+                "\n"+"Nombre; "+per1.getNombre()+
+                "\n"+"Apelido; "+per1.getApellido();
+
+    }
+
+}
