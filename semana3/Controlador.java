e3df1c35fe05156acdd33f91bb8b22509f0f2ac3
GabrielGregorio <gabriel95jk@gmail.com>
2022-06-08 19:20:26 -0300

    borro carpetas

--- semana3/Controlador.java
+++
@@ -1,8 +0,0 @@
-package semana3;
-
-public class Controlador {
-    public static void main(String[] args) {
-        Persona Persona1=Vista.EntregarPersona();
-        System.out.println(Vista.MostarPersona(Persona1));
-    }
-}
